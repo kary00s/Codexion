c0afc9171e309be2d630886fc94e66b02954eae3
Karim <karimnahiz18@gmail.com>
2026-06-23 20:35:57 +0100

    kanahoz

--- tests.c
+++ tests.c
@@ -1,44 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include<pthread.h>
-#include<time.h>
-#include<sys/time.h>
+#include<stdio.h>
 #include<unistd.h>
-pthread_cond_t cond;
-pthread_mutex_t mutex ;
-// void *routine(void *args)
-// {
-
-//     while (1)
-//     {        
-//         pthread_mutex_lock(&mutex);
-//         printf("created succeffully\n");
-//         pthread_mutex_unlock(&mutex);
-//         printf("is running\n");
-//         pthread_cond_wait(&cond, &mutex);
-//         sleep(1);   
-//     }
-//     return NULL;
-// }
-// int main(void)
-// {
-//     pthread_t thread;
-//     pthread_t thread1;
-
-//     pthread_create(&thread, NULL, routine, NULL);
-//     pthread_create(&thread1, NULL, routine, NULL);
-//     pthread_join(thread, NULL);
-//     pthread_join(thread1, NULL);
-    
-//     return 0;
-// }
-long get_time_ms()
+#include<pthread.h>
+#include <sys/time.h>
+typedef struct s_node
 {
-	struct timeval time;
-	gettimeofday(&time, NULL);
-	return (time.tv_sec * 1000) + (time.tv_usec / 1000) ;
-}
+	int node_id;
+	time_t last_compile;
+} t_node;
+ 
+typedef struct s_heap
+{
+	t_node **nodes;
+	int size;
+	int capacity;
+	pthread_mutex_t mutex_heap;
+} t_heap;
+
 int main()
 {
-    printf("%ld\n", get_time_ms());
+	t_heap	*heap;
+	heap = malloc(sizeof(t_heap));
+	heap->size = 0;
+	heap->capacity = 3;
+	heap->nodes = malloc(sizeof(t_node *) * heap->capacity);
+	pthread_mutex_init(&heap->mutex_heap, NULL);
+
+	t_node *node1;
+	node1->last_compile = 10;
+	node1->node_id = 1;
+
+	t_node *node2;
+	node2->last_compile = 20;
+	node2->node_id = 2;
+	
+	t_node *node3;
+	node3->last_compile = 30;
+	node3->node_id = 3;
+	
+	insert_to_heap(node1, heap);
 }
